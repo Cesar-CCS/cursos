ee054f8e55b11f1862ec6c1e5db7f45a96ae8eef
Cesar-CCS <cesarcastro_s@outlook.com>
2024-01-21 16:56:23 -0600

    Screens creadas y Barril Actualizado

---
+++ Flutter/Flutter/Demo/widgets_app/lib/presentation/screens/progress/progress_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class PogressScreen extends StatelessWidget {
+  static const name = 'progress_screen';
+
+  const PogressScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

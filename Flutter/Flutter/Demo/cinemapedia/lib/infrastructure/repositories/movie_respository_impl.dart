a99040c0b85b10c00d0667776646746e341d5399
Cesar-CCS <cesarcastro_s@outlook.com>
2024-01-29 18:03:54 -0600

    Cinema pedia consume now playing

---
+++ Flutter/Flutter/Demo/cinemapedia/lib/infrastructure/repositories/movie_respository_impl.dart
@@ -0,0 +1,13 @@
+import 'package:cinemapedia/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_respository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+  final MovieDataSource datasource;
+
+  MovieRepositoryImpl(this.datasource);
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+}
--- Flutter/Flutter/Demo/cinemapedia/lib/presentation/screens/movies/home_screen.dart
+++ Flutter/Flutter/Demo/cinemapedia/lib/presentation/screens/movies/home_screen.dart
@@ -1,5 +1,7 @@
-import 'package:cinemapedia/config/constants/enviroment.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/presentation/providers/movies/movies_providers.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 //import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,10 +11,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text(Enviroment.theMovieDbKey),
-      ),
-    );
+    return _HomeView();
+  }
+}
+
+class _HomeView extends ConsumerStatefulWidget {
+  const _HomeView();
+
+  @override
+  _HomeViewState createState() => _HomeViewState();
+}
+
+class _HomeViewState extends ConsumerState<_HomeView> {
+  @override
+  void initState() {
+    super.initState();
+
+    ref.read(nowPlayingMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
+
+    return ListView.builder(
+        itemCount: nowPlayingMovies.length,
+        itemBuilder: (context, index) {
+          final movie = nowPlayingMovies[index];
+          return ListTile(
+            title: Text(movie.title),
+          );
+        });
   }
 }

320af5f4edcbc2f9097545a41918c542e9af122d
Cesar-CCS <cesarcastro_s@outlook.com>
2024-03-12 18:26:24 -0500

    Composicion - Curso Angular

--- Angular/01-typescript-intro/src/topics/08-classes.ts
+++ Angular/01-typescript-intro/src/topics/08-classes.ts
@@ -1,14 +1,46 @@
 export class Person {
-    public name: string;
-    private address: string;
+    // public name: string;
+    // private address: string;
 
-    constructor() {
-        this.name = 'Fernando';
-        this.address = 'New York';
-    }
-
+    //De esta manera se genera un constructor omitiendo la asignacion
+    //ya que se hace de forma directa y se iguala a los valores por defecto
+    constructor(
+        public  firstName: string, 
+        public lastName: string,
+        private address: string = 'No Address'
+    ) {}
 }
 
-const ironman = new Person();
+// export class Hero extends Person {
+
+//     constructor(
+//         public alterEgo: string,
+//         public age: number,
+//         public realName:string
+//     ) {
+//         super( realName, 'New York');
+//     }
+// }
+
+//Composicion
+export class Hero {
+
+    //public person: Person;
+
+    constructor(
+        public alterEgo: string,
+        public age: number,
+        public realName:string,
+        public person: Person
+    ) {
+        
+        //this.person = new Person(realName);
+
+    }
+}
+
+const tony = new Person('Tony','Stark',' New York');
+
+const ironman = new Hero('Ironman',45,'Tony', tony);
 
 console.log(ironman);
